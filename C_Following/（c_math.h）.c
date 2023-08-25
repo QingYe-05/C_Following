6b10db6ff61cef01ce746de449503f1682083abd
清新 耿 <1105881275@qq.com>
2023-08-26 00:48:37 +0800

    库函数初步学习结束

--- C_Following/（c_math.h）.c
+++ C_Following/（c_math.h）.c
@@ -10,6 +10,7 @@ ceil()：向上取整，返回不小于给定浮点型数的最小整数值。
 floor()：向下取整，返回不大于给定浮点型数的最大整数值。
 round()：四舍五入，返回给定浮点型数最接近的整数值。
 fmod()：浮点型数求模运算，返回两个浮点型数相除的余数。
+
 三角函数：
 
 sin()：求正弦值。
@@ -31,4 +32,634 @@ max()：返回两个参数中较大的一个。
 min()：返回两个参数中较小的一个。
 rand()：生成随机数。
 srand()：设置随机数生成的种子值。
+*/
+
+/*
+1>fabs
+double fabs (double x);      
+float fabs (float x);
+long double fabs (long double x);
+计算绝对值
+
+x
+返回x的绝对值:|x|。
+
+#include <stdio.h>      // printf
+#include <math.h>       // fabs
+
+
+int main()
+{
+	printf("The absolute value of 3.1416 is %f\n", fabs(3.1416));
+	printf("The absolute value of -10.6 is %f\n", fabs(-10.6));
+	return 0;
+}
+//Output:
+//
+//The absolute value of 3.1416 is 3.141600
+//The absolute value of - 10.6 is 10.600000
+
+*/
+
+/*
+2>ceil
+double ceil (double x);      
+float ceil (float x);
+long double ceil (long double x);
+
+向上取整
+向上舍入x，返回不小于x的最小整数值。
+
+返回值
+不小于x的最小整数值(作为浮点值)。
+
+#include <stdio.h>      // printf
+#include <math.h>       // ceil
+
+int main()
+{
+	printf("ceil of 2.3 is %.1f\n", ceil(2.3));
+	printf("ceil of 3.8 is %.1f\n", ceil(3.8));
+	printf("ceil of -2.3 is %.1f\n", ceil(-2.3));
+	printf("ceil of -3.8 is %.1f\n", ceil(-3.8));
+	return 0;
+}
+
+//ceil of 2.3 is 3.0
+//ceil of 3.8 is 4.0
+//ceil of - 2.3 is - 2.0
+//ceil of - 3.8 is - 3.0
+
+*/
+
+
+/*
+3>floor
+double floor (double x);      
+float floor (float x);
+long double floor (long double x);
+
+向下取整
+向下舍入x，返回不大于x的最大整数值。
+
+返回值
+x的值向下舍入(作为浮点值)。
+
+#include <stdio.h>      // printf
+#include <math.h>      // floor
+
+int main()
+{
+	printf("floor of 2.3 is %.1lf\n", floor(2.3));
+	printf("floor of 3.8 is %.1lf\n", floor(3.8));
+	printf("floor of -2.3 is %.1lf\n", floor(-2.3));
+	printf("floor of -3.8 is %.1lf\n", floor(-3.8));
+	return 0;
+}
+
+//floor of 2.3 is 2.0
+//floor of 3.8 is 3.0
+//floor of - 2.3 is - 3.0
+//floor of - 3.8 is - 4.0
+*/
+
+/*
+4>round
+ double round  (double x);      
+ float roundf (float x);
+ long double roundl (long double x);
+
+四舍五入
+返回最接近x的整数值，中间情况从零四舍五入。
+
+#include <stdio.h>      // printf 
+#include <math.h>       // round, floor, ceil, trunc 
+
+int main()
+{
+	const char* format = "%.1f \t%.1f \t%.1f \t%.1f \t%.1f\n";
+	printf("value\tround\tfloor\tceil\ttrunc\n");
+	printf("-----\t-----\t-----\t----\t-----\n");
+	printf(format, 2.3, round(2.3), floor(2.3), ceil(2.3), trunc(2.3));
+	printf(format, 3.8, round(3.8), floor(3.8), ceil(3.8), trunc(3.8));
+	printf(format, 5.5, round(5.5), floor(5.5), ceil(5.5), trunc(5.5));
+	printf(format, -2.3, round(-2.3), floor(-2.3), ceil(-2.3), trunc(-2.3));
+	printf(format, -3.8, round(-3.8), floor(-3.8), ceil(-3.8), trunc(-3.8));
+	printf(format, -5.5, round(-5.5), floor(-5.5), ceil(-5.5), trunc(-5.5));
+	return 0;
+}
+//初始值	四舍五入  向下取整  向上取整 舍零
+//value   round   floor   ceil    trunc
+//-----   -----   -----   ----    -----
+//2.3     2.0     2.0     3.0     2.0
+//3.8     4.0     3.0     4.0     3.0
+//5.5     6.0     5.0     6.0     5.0
+//-2.3    -2.0    -3.0    -2.0    -2.0
+//-3.8    -4.0    -4.0    -3.0    -3.0
+//-5.5    -6.0    -6.0    -5.0    -5.0
+
+*/
+/*
+5>trunc
+double trunc (double x);      
+float truncf (float x);
+long double truncl (long double x);
+舍零运算
+见上述4的例子运算
+*/
+
+
+/*
+6>fmod
+double fmod (double numer, double denom);      
+float fmod (float numer, float denom);
+long double fmod (long double numer, long double denom);
+求模运算  -- 计算除法余数
+
+numer
+商分子的值。
+denom
+商分母的值
+
+返回值
+除法运算的余数
+
+#include <stdio.h>      // printf
+#include <math.h>       // fmod
+
+int main()
+{
+	printf("fmod of 5.3 / 2 is %f\n", fmod(5.3, 2));
+	printf("fmod of 18.5 / 4.2 is %f\n", fmod(18.5, 4.2));
+	return 0;
+}
+//Output:
+//
+//fmod of 5.3 / 2 is 1.300000
+//fmod of 18.5 / 4.2 is 1.700000
+
+*/
+
+
+//三角函数：
+//
+//sin()：求正弦值。
+//cos()：求余弦值。
+//tan()：求正切值。
+//asin()：求反正弦值。
+//acos()：求反余弦值。
+//atan()：求反正切值。
+
+/*
+7>sin
+ double sin (double x);      
+ float sin (float x);
+ long double sin (long double x);
+
+ 计算正弦
+返回x弧度角的正弦值。
+
+x
+用弧度表示角度的值。
+1弧度等于180/PI度。
+
+#include <stdio.h>      // printf
+#include <math.h>       // sin
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 30.0;
+
+	//六分之PI
+	result = sin(param * PI / 180);
+	printf("The sine of %f degrees is %f.\n", param, result);
+	return 0;
+}
+//Output:
+//
+//The sine of 30.000000 degrees is 0.500000.
+*/
+
+
+/*
+8>cos
+ double cos (double x);      
+ float cos (float x);
+ long double cos (long double x);
+计算余弦
+返回x弧度角的余弦值。
+
+x
+用弧度表示角度的值。
+1弧度等于180/PI度。
+
+#include <stdio.h>      // printf
+#include <math.h>       // cos
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 60.0;
+	result = cos(param * PI / 180.0);
+	printf("The cosine of %f degrees is %f.\n", param, result);
+	return 0;
+}
+//Output:
+//
+//The cosine of 60.000000 degrees is 0.500000.
+
+*/
+
+
+/*
+9>tan
+ double tan (double x);      
+ float tan (float x);
+ long double tan (long double x);
+计算切线
+返回x弧度角的正切值。
+
+x
+表示角度的值，用弧度表示。
+1弧度等于180/PI度。
+
+#include <stdio.h>      // printf
+#include <math.h>       // tan
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 45.0;
+	result = tan(param * PI / 180.0);
+	printf("The tangent of %f degrees is %f.\n", param, result);
+	return 0;
+}
+//Output:
+//
+//The tangent of 45.000000 degrees is 1.000000.
+
+*/
+
+
+/*
+10>asin
+
+返回值
+主arcsinx，在区间[- /2，+ /2]弧度内。
+1弧度等于180/PI度。
+
+
+#include <stdio.h>      // printf
+#include <math.h>       // asin
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 0.5;
+	result = asin(param) * 180.0 / PI;
+	printf("The arc sine of %f is %f degrees\n", param, result);
+	return 0;
+}
+Output:
+
+The arc sine of 0.500000 is 30.000000 degrees.
+*/
+
+
+
+/*
+11>acos
+计算反余弦
+返回以弧度表示的arccosx的主值。
+
+
+#include <stdio.h>      // printf 
+#include <math.h>       // acos 
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 0.5;
+	result = acos(param) * 180.0 / PI;
+	printf("The arc cosine of %f is %f degrees.\n", param, result);
+	return 0;
+}
+
+//Output:
+//The arc cosine of 0.500000 is 60.000000 degrees.
+*/
+
+/*
+12>atan
+double atan (double x);      
+float atan (float x);
+long double atan (long double x);
+计算弧切线 反正切
+返回x的arctan的主值，以弧度表示。
+
+返回值
+x的主arctan，在区间[- pi/2，+ pi/2]弧度内。
+1弧度等于180/PI度。
+
+#include <stdio.h>      // printf
+#include <math.h>       // atan
+
+#define PI 3.14159265
+
+int main()
+{
+	double param, result;
+	param = 1.0;
+	result = atan(param) * 180 / PI;
+	printf("The arc tangent of %f is %f degrees\n", param, result);
+	return 0;
+}
+//输出:
+//1.000000的弧切是45.000000度。
+*/
+
+
+
+//指数与对数：
+//
+//pow()：求幂运算，返回给定底数的给定指数次幂。
+//sqrt()：求平方根。
+//exp()：求指数函数，返回e的给定指数次幂。
+//log()：求以e为底的自然对数。
+//log10()：求以10为底的对数。
+// 
+
+/*13>pow
+double pow (double base, double exponent);      
+float pow (float base, float exponent);
+long double pow (long double base, long double exponent);     
+double pow (double base, int exponent);
+long double pow (long double base, int exponent);
+
+提高权
+返回基数的幂指数:
+
+base
+基础值。
+exponent
+指数的值。
+
+返回值
+取底的幂指数的结果。
+如果基数是有限负的，并且指数是有限的但不是整数，则会导致域错误。
+如果基数和指数都为零，在某些实现上也可能导致域错误。
+如果base为零且exponent为负，则可能导致域错误或极点错误(或没有错误，取决于库实现)。
+如果结果太大或太小而无法用返回类型的值表示，则该函数还可能导致范围错误。
+
+#include <stdio.h>      // printf
+#include <math.h>       // pow
+
+int main()
+{
+	printf("7 ^ 3 = %f\n", pow(7.0, 3.0));
+	printf("4.73 ^ 12 = %f\n", pow(4.73, 12.0));
+	printf("32.01 ^ 1.54 = %f\n", pow(32.01, 1.54));
+	return 0;
+}
+
+//Output:
+//
+//7 ^ 3 = 343.000000
+//4.73 ^ 12 = 125410439.217423
+//32.01 ^ 1.54 = 208.036691
+
+*/
+
+
+/*14>sqrt
+double sqrt (double x);      
+float sqrt (float x);
+long double sqrt (long double x);
+
+计算平方根
+返回根号x。
+
+返回值
+√x。
+如果x为负，则出现域错误:
+
+#include <stdio.h>      // printf
+#include <math.h>       // sqrt
+
+int main()
+{
+	double param, result;
+	param = 1024.0;
+	result = sqrt(param);
+	printf("sqrt(%f) = %f\n", param, result);
+	return 0;
+}
+//Output:
+//
+//sqrt(1024.000000) = 32.000000
+
+*/
+
+
+/*15>exp
+double exp (double x);      
+float exp (float x);
+long double exp (long double x);
+
+计算指数函数
+返回x的以e为底的指数函数，即e的x次方:。
+
+返回值
+x的指数值。
+如果结果的大小太大，无法用返回类型的值表示，
+则该函数返回带有正确符号的HUGE_VAL
+
+#include <stdio.h>      // printf
+#include <math.h>       // exp
+
+int main()
+{
+	double param, result;
+	param = 5.0;
+	result = exp(param);
+	printf("The exponential value of %f is %f.\n", param, result);
+	return 0;
+}
+//Output:
+//e的5次方
+//The exponential value of 5.000000 is 148.413159.
+*/
+
+
+
+/*16>log
+double log (double x);      
+float log (float x);
+long double log (long double x);
+计算自然对数
+自然对数是以e为底的对数:自然指数函数(exp)的倒数。
+
+返回值
+x的自然对数。
+如果x是负的，它会导致一个域错误。
+如果x为零，则可能导致极点错误(取决于库实现)。
+
+#include <stdio.h>      // printf
+#include <math.h>       // log
+
+int main()
+{
+	double param, result;
+	param = 2.7;
+	result = log(param);//
+	printf("log(%f) = %f\n", param, result);
+	return 0;
+}
+//log(2.700000) = 0.993252
+
+*/
+
+/*17>log10
+ouble log10 (double x);      
+float log10 (float x);
+long double log10 (long double x);
+
+计算公对数
+返回x的公数对数(以10为底)。
+
+#include <stdio.h>      // printf 
+#include <math.h>       // log10 
+
+int main()
+{
+	double param, result;
+	param = 1000.0;
+	result = log10(param);
+	printf("log10(%f) = %f\n", param, result);
+	return 0;
+}
+
+//log10(1000.000000) = 3.000000
+
+*/
+
+
+
+//其他常用函数：
+//
+//fmax()：返回两个参数中较大的一个。
+//fmin()：返回两个参数中较小的一个。
+//rand()：生成随机数。
+//srand()：设置随机数生成的种子值。
+#include<math.h>
+int main() {
+
+	return 0;
+}
+/*18>fmax
+double fmax (double x, double y);
+
+最大值
+返回其参数中较大的一个:x或y。
+
+
+返回值
+其参数的最大数值。
+
+#include <stdio.h>      // printf
+#include <math.h>       // fmax
+
+int main()
+{
+	printf("fmax (100.0, 1.0) = %f\n", fmax(100.0, 1.0));
+	printf("fmax (-100.0, 1.0) = %f\n", fmax(-100.0, 1.0));
+	printf("fmax (-100.0, -1.0) = %f\n", fmax(-100.0, -1.0));
+	return 0;
+}
+//Output:
+//
+//fmax(100.0, 1.0) = 100.000000
+//fmax(-100.0, 1.0) = 1.000000
+//fmax(-100.0, -1.0) = -1.000000
+
+*/
+
+
+/*19>fmin
+double fmin (double x, double y);
+返回值
+其参数的最小数值。
+
+include <stdio.h>      // printf 
+#include <math.h>       // fmin 
+
+int main()
+{
+	printf("fmin (100.0, 1.0) = %f\n", fmin(100.0, 1.0));
+	printf("fmin (-100.0, 1.0) = %f\n", fmin(-100.0, 1.0));
+	printf("fmin (-100.0, -1.0) = %f\n", fmin(-100.0, -1.0));
+	return 0;
+}
+//Output:
+
+//fmin (100.0, 1.0) = 1.000000
+//fmin (-100.0, 1.0) = -100.000000
+//fmin (-100.0,-1.0) = -100.000000
+*/
+
+/*
+20>modf
+double modf (double x, double* intpart);
+
+将浮点值拆分为小数和整数部分。
+整数部分存储在intpart所指向的对象中，小数部分由函数返回。
+这两部分的符号都是x。
+
+参数
+x
+将浮点值分解为多个部分。
+intpart
+指向一个对象(与x相同类型)的指针，其中整型部分以与x相同的符号存储。
+
+返回值
+x的小数部分，同样的符号。
+
+#include <stdio.h>      // printf
+#include <math.h>       // modf
+
+int main()
+{
+	double param, fractpart, intpart;
+
+	param = 3.14159265;
+	fractpart = modf(param, &intpart);
+	printf("%f = %f + %f \n", param, intpart, fractpart);
+	return 0;
+}
+//Output:
+//
+//3.141593 = 3.000000 + 0.141593
+*/
+
+
+/*20>rand
+
+
+
+*/
+/*21>srand
+
+
+
 */
--- C_Following/（c_string.h）.c
+++ C_Following/（c_string.h）.c
@@ -1,26 +1,183 @@
 #define _CRT_SECURE_NO_WARNINGS
 /*
 1>memset
+void * memset ( void * ptr, int value, size_t num );
+将ptr指向的内存块的前num个字节设置为指定的值(解释为unsigned char)
+将缓冲区设置为指定字符
+
+ptr
+指向要填充的内存块的指针。
+value
+需要设置的值。该值作为int类型传递，但函数使用该值的unsigned char转换来填充内存块。
+num
+要设置为该值的字节数。
+Size_t是一个无符号整型
+
+
+#include<stdio.h>
+#include<string.h>
+int main() {
+
+	char str[] = "almost every programmer should know memset!";
+	memset(str, '-', 6);//使用memset()函数将字符串 str 的前6个字符都设置为 '-'。
+	puts(str);
+
+	return 0;
+}
+//------every programmer should know memset!
 
 */
 
 
 /*
 2>strcat
+char * strcat ( char * destination, const char * source );
+连接字符串(Concatenate strings)
+
+strcat函数将Source附加到Destination后，并以空字符结束结果字符串。
+Source的初始字符覆盖Destination的终止null字符。
+复制或追加字符串时不执行溢出检查。
+如果源字符串和目标字符串重叠，则strcat的行为未定义。
+
+目的地和源头不得重叠
+
+destination
+指向目标数组的指针，该数组应包含一个C字符串，并且足够大以包含连接的结果字符串。
+source
+要追加的C字符串。这应该不重叠的目的地。
+
+返回值
+返回目标字符串
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str[80];
+	//使用strcpy()函数将字符串 "these "复制到数组 str 中。
+	strcpy(str, "these ");
+	//使用strcat()函数将字符串 "strings "拼接到 str 后面。
+	strcat(str, "strings ");
+	strcat(str, "are ");
+	strcat(str, "concatenated.");
+	puts(str);
+	return 0;
+}
+//Output:
+//these strings are concatenated.
 */
 
 
 /*
 3>strcmp
+int strcmp ( const char * str1, const char * str2 );
 
+将 C 字符串 str1 与 C 字符串 str2 进行比较。
+
+此函数开始比较每个字符串的第一个字符。
+如果它们彼此相等，则继续以下对，直到字符不同或达到终止空字符。
+
+返回值
+返回一个整数值，该值指示字符串之间的关系：
+返回值	表明
+<0		第一个不匹配的字符在 PTR1 中的值低于 PTR2 中的值
+0		两个字符串的内容相等
+>0		第一个不匹配的字符在 PTR1 中的值大于在 PTR2 中的值
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char key[] = "apple";
+	char buffer[80];
+	do {
+		printf("Guess my favorite fruit? ");
+		fflush(stdout);
+		scanf("%79s", buffer);
+	} while (strcmp(key, buffer) != 0);
+	puts("Correct answer!");
+	return 0;
+}
+//output:
+//Guess my favourite fruit ? orange
+//Guess my favourite fruit ? apple
+//Correct answer!
 */
+
 
 /*
 4>strcpy
+char * strcpy ( char * destination, const char * source );
+将source指向的C字符串复制到destination指向的数组中，
+包括结束的null字符(并在该点停止)。
+
+参数
+destination
+指向要在其中复制内容的目标数组的指针。
+source
+要复制的C字符串。
+
+译文：
+
+返回目标数组的值。
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str1[] = "Sample string";
+	char str2[40];
+	char str3[40];
+	strcpy(str2, str1);
+	strcpy(str3, "copy successful");
+	printf("str1: %s\nstr2: %s\nstr3: %s\n", str1, str2, str3);
+	return 0;
+}
+//Output:
+//
+//str1: Sample string
+//str2 : Sample string
+//str3 : copy successful
 
 */
 
 
 /*
 5>strlen
+size_t strlen ( const char * str );
+获取字符串长度
+C字符串的长度由结束的空字符决定:
+C字符串的长度等于字符串开头和结束的空字符之间的字符数
+(不包括结束的空字符本身)。
+
+char mystr[100]="test string";
+定义了一个长度为100个字符的字符数组，但是mystr初始化的C字符串的长度只有11个字符。
+因此，sizeof(mystr)的结果是100，而strlen(mystr)的结果是11。
+sizeof()计算数组的大小
+strlen()计算字符串的长度
+
+
+返回值
+字符串的长度
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char szInput[256];
+	printf("Enter a sentence: ");
+	gets(szInput);
+	printf("The sentence entered is %u characters long.\n", (unsigned)strlen(szInput));
+	return 0;
+}
+//Output:
+//
+//Enter sentence : just testing
+//The sentence entered is 12 characters long.
 */
+
